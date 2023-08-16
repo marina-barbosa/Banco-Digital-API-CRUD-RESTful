d820a00f901aaa815b21579fcfe22ee51d80399c
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-08-16 01:24:10 -0300

    add criar conta

--- src/bancodedados.js
+++ src/bancodedados.js
@@ -1,4 +1,4 @@
-module.exports = {
+let dados = {
     banco: {
         nome: 'Cubos Bank',
         numero: '123',
@@ -7,7 +7,7 @@ module.exports = {
     },
     contas: [
         {
-            "numero": "1",
+            "numero": 1,
             "saldo": 0,
             "usuario": {
                 "nome": "Foo Bar",
@@ -19,7 +19,7 @@ module.exports = {
             }
         },
         {
-            "numero": "2",
+            "numero": 2,
             "saldo": 1000,
             "usuario": {
                 "nome": "Foo Bar 2",
@@ -35,3 +35,43 @@ module.exports = {
     saques: [],
     transferencias: []
 }
+
+module.exports = dados;
+
+// module.exports = {
+//     banco: {
+//         nome: 'Cubos Bank',
+//         numero: '123',
+//         agencia: '0001',
+//         senha: 'Cubos123Bank'
+//     },
+//     contas: [
+//         {
+//             "numero": "1",
+//             "saldo": 0,
+//             "usuario": {
+//                 "nome": "Foo Bar",
+//                 "cpf": "00011122233",
+//                 "data_nascimento": "2021-03-15",
+//                 "telefone": "71999998888",
+//                 "email": "foo@bar.com",
+//                 "senha": "1234"
+//             }
+//         },
+//         {
+//             "numero": "2",
+//             "saldo": 1000,
+//             "usuario": {
+//                 "nome": "Foo Bar 2",
+//                 "cpf": "00011122234",
+//                 "data_nascimento": "2021-03-15",
+//                 "telefone": "71999998888",
+//                 "email": "foo@bar2.com",
+//                 "senha": "12345"
+//             }
+//         }
+//     ],
+//     depositos: [],
+//     saques: [],
+//     transferencias: []
+// }
--- src/controladores/banco.js
+++ src/controladores/banco.js
@@ -1,9 +1,72 @@
 const { banco, contas, depositos, saques, transferencias } = require('../bancodedados.js');
+let numeroContas = 2;
 
 const listarContas = (req, res) => {
     return res.json(contas);
 }
 
+const criarConta = (req, res) => {
+    const { nome, cpf, data_nascimento, telefone, email, senha } = req.body;
+
+
+
+    if (!nome) {
+        return res.status(400).json({ "mensagem": "O nome deve ser informado." });
+    }
+
+    if (!cpf) {
+        return res.status(400).json({ "mensagem": "O CPF deve ser informado." });
+    }
+
+    if (!data_nascimento) {
+        return res.status(400).json({ "mensagem": "A data de nascimento deve ser informada." });
+    }
+
+    if (!telefone) {
+        return res.status(400).json({ "mensagem": "O telefone deve ser informado." });
+    }
+
+    if (!email) {
+        return res.status(400).json({ "mensagem": "O email deve ser informado." });
+    }
+
+    if (!senha) {
+        return res.status(400).json({ "mensagem": "O senha deve ser informada." });
+    }
+
+    if (contas.find(conta => conta.usuario.nome === nome)) {
+        return res.status(400).json({ "mensagem": "O nome informado já existe cadastrado!" });
+    }
+
+    if (contas.find(conta => conta.usuario.cpf === cpf)) {
+        return res.status(400).json({ "mensagem": "O CPF informado já existe cadastrado!" });
+    }
+
+    if (contas.find(conta => conta.usuario.email === email)) {
+        return res.status(400).json({ "mensagem": "O email informado já existe cadastrado!" });
+    }
+
+    numeroContas++;
+
+    const novaConta = {
+        "numero": numeroContas,
+        "saldo": 0,
+        "usuario": {
+            nome,
+            cpf,
+            data_nascimento,
+            telefone,
+            email,
+            senha
+        }
+    }
+
+    contas.push(novaConta);
+
+    return res.status(201).send();
+}
+
 module.exports = {
     listarContas,
+    criarConta,
 }
