93e8035bd38cd9d385d9502a537f45531e546b4e
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-08-16 15:09:53 -0300

    add excluir conta

--- src/controladores/banco.js
+++ src/controladores/banco.js
@@ -1,4 +1,4 @@
-const { banco, contas, depositos, saques, transferencias } = require('../bancodedados.js');
+let { banco, contas, depositos, saques, transferencias } = require('../bancodedados.js');
 let numeroContas = 2;
 
 const listarContas = (req, res) => {
@@ -35,7 +35,7 @@ const criarConta = (req, res) => {
 }
 
 const atualizarUsuario = (req, res) => {
-    const { numeroConta } = req.params;
+    const numeroConta = Number(req.params.numeroConta);
     const { nome, cpf, data_nascimento, telefone, email, senha } = req.body;
 
     if (isNaN(numeroConta)) {
@@ -43,7 +43,7 @@ const atualizarUsuario = (req, res) => {
     }
 
     const conta = contas.find((conta) => {
-        return conta.numero === Number(numeroConta);
+        return conta.numero === numeroConta;
     });
 
     if (!conta) {
@@ -69,21 +69,45 @@ const atualizarUsuario = (req, res) => {
 }
 
 const excluirConta = (req, res) => {
-    const { numeroConta } = req.params;
+    const numeroConta = Number(req.params.numeroConta);
+
+    if (isNaN(numeroConta)) {
+        return res.status(400).json({ mensagem: 'Número de conta inválido.' });
+    }
+
+    const conta = contas.find((conta) => {
+        return conta.numero === numeroConta;
+    });
+
+    if (!conta) {
+        return res.status(404).json({ mensagem: 'Conta não encontrada.' });
+    };
+
+    if (conta.saldo !== 0) {
+        return res.status(403).json({ mensagem: 'A conta só pode ser removida se o saldo for zero!' });
+    }
+
+    contas = contas.filter((conta) => {
+        return conta.numero !== numeroConta;
+    });
+
+    return res.status(204).json();
 }
 
+// function verificaNumeroConta(numeroConta) { }
+
 function verificaDados(nome, cpf, data_nascimento, telefone, email, senha) {
-    if (!nome || nome === '') {
+    if (!nome || nome.trim() === '') {
         erro = { "mensagem": "O nome deve ser informado." };
-    } else if (!cpf || cpf === '') {
+    } else if (!cpf || cpf.trim() === '') {
         erro = { "mensagem": "O CPF deve ser informado." };
-    } else if (!data_nascimento || data_nascimento === '') {
+    } else if (!data_nascimento || data_nascimento.trim() === '') {
         erro = { "mensagem": "A data de nascimento deve ser informada." };
-    } else if (!telefone || telefone === '') {
+    } else if (!telefone || telefone.trim() === '') {
         erro = { "mensagem": "O telefone deve ser informado." };
-    } else if (!email || email === '') {
+    } else if (!email || email.trim() === '') {
         erro = { "mensagem": "O email deve ser informado." };
-    } else if (!senha || senha === '') {
+    } else if (!senha || senha.trim() === '') {
         erro = { "mensagem": "O senha deve ser informada." };
     } else if (contas.find(conta => conta.usuario.nome === nome)) {
         erro = { "mensagem": "O nome informado já existe cadastrado!" };
