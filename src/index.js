7f4de9ed02d1ee9fe0cc7898ed03d431d0833197
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-08-15 23:58:47 -0300

    inicio do desafio

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Criar conta bancária
+// Listar contas bancárias
+// Atualizar os dados do usuário da conta bancária
+// Excluir uma conta bancária
+// Depósitar em uma conta bancária
+// Sacar de uma conta bancária
+// Transferir valores entre contas bancárias
+// Consultar saldo da conta bancária
+// Emitir extrato bancário
+
